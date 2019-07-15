24009be2d01d88e4f4a4aa086dd1b1ac488ebb1b
KloeAn <KLOE.AN@OOCL.COM>
2019-07-15 13:39:10 +0800

    [kloe][feat:should_return_other_expense_type_if_project_is_external_and_has_other_name]

--- src/test/java/ExpenseService/ExpenseServiceTest.java
+++ src/test/java/ExpenseService/ExpenseServiceTest.java
@@ -45,12 +45,12 @@ class ExpenseServiceTest {
 
     @Test
     void should_return_other_expense_type_if_project_is_external_and_has_other_name() throws UnexpectedProjectTypeException {
-//        // given
-//        Project project=new Project(ProjectType.EXTERNAL,"Project C");
-//        // when
-//        ExpenseType projectType=new ExpenseService().getExpenseCodeByProjectTypeAndName(project);
-//        // then
-//        assertSame(ExpenseType.OTHER_EXPENSE,projectType);
+        // given
+        Project project=new Project(ProjectType.EXTERNAL,"Project C");
+        // when
+        ExpenseType projectType=new ExpenseService().getExpenseCodeByProjectTypeAndName(project);
+        // then
+        assertSame(ExpenseType.OTHER_EXPENSE,projectType);
     }
 
     @Test
